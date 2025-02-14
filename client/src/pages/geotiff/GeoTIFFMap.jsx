9cd66f419845c4372a47b8e052b22dc64319299f
Stefan Eberlein <stefan.eberlein@fhnw.ch>
2025-02-14 19:04:56 +0100

    simplify geotiff example
    - remove swiss data code because maplibre does not support other projections

--- client/src/pages/geotiff/GeoTIFFMap.jsx
+++ client/src/pages/geotiff/GeoTIFFMap.jsx
@@ -12,20 +12,13 @@ function GeoTIFFMap({ opacity }) {
     mapRef.current = new maplibregl.Map({
       container: "geotiff-map-container", // html container id
       style: "https://geoserveis.icgc.cat/contextmaps/icgc_mapa_base_gris_simplificat.json", //stylesheet location
-      // style: "https://vectortiles.geo.admin.ch/styles/ch.swisstopo.lightbasemap.vt/style.json", //stylesheet location
       center: [1.83369, 41.5937], // starting position
-      // center: [7.642323, 47.534655], // starting position
       zoom: 15.5, // starting zoom
-      // maxBounds: [
-      //   [4, 43],
-      //   [13, 50],
-      // ],
     });
     maplibregl.addProtocol("cog", cogProtocol);
     mapRef.current.once("load", () => {
       mapRef.current.addSource("geotiff", {
         type: "raster",
-        // url: "cog://https://data.geo.admin.ch/ch.swisstopo.pixelkarte-farbe-pk50.noscale/swiss-map-raster50_2023_213/swiss-map-raster50_2023_213_krel_2.5_2056.tif",
         url: "cog://https://labs.geomatico.es/maplibre-cog-protocol/data/image.tif",
         tileSize: 256,
       });
