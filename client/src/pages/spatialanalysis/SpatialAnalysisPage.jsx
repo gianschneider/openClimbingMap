50f105ca8b9bc439f675d04e6d780c75e28fa8a6
Stefan Eberlein <stefan.eberlein@fhnw.ch>
2025-02-19 17:24:56 +0100

    add explainer text for spatial analysis

--- client/src/pages/spatialanalysis/SpatialAnalysisPage.jsx
+++ client/src/pages/spatialanalysis/SpatialAnalysisPage.jsx
@@ -6,6 +6,8 @@ function SpatialAnalysisPage() {
   const [bufferDistance, setBufferDistance] = useState(50);
   return (
     <div id="spatialanalysis-content">
+      <p>Roads are a useful proxy for emissions. This example loads the SwissTopo vector tiles basemap and uses the included road data to highlight areas that are far away from roads and thus likely less affected by traffic emissions.</p>
+        <p>
       <label>
         Buffer distance around roads in meters:
         <input
@@ -13,7 +15,7 @@ function SpatialAnalysisPage() {
           type="number"
           value={bufferDistance}
         />
-      </label>
+      </label></p>
       <SpatialAnalysisMap bufferDistance={bufferDistance} />
     </div>
   );
