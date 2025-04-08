ff8443f826e897dd191514ab07a03907f5f04a22
Gian Schneider <gianschneider12@bluewin.ch>
2025-04-08 16:12:45 +0200

    geocover routing

--- client/src/pages/basemap/layers/BackgroundLayers.js
+++ client/src/pages/basemap/layers/BackgroundLayers.js
@@ -1,7 +1,24 @@
 import TileLayer from "ol/layer/Tile.js";
 import TileWMS from "ol/source/TileWMS.js";
+import VectorLayer from "ol/layer/Vector.js";
+import VectorSource from "ol/source/Vector.js";
+import GeoJSON from "ol/format/GeoJSON.js";
 
-const extent = [2420000, 130000, 2900000, 1350000]; // Definiere das Kartenextens
+// Definiere das Kartenextens
+const extent = [2420000, 130000, 2900000, 1350000];
+
+//Geocover Layer
+
+export const createGeocoverLayer = () => {
+  return new VectorLayer({
+    source: new VectorSource({
+      format: new GeoJSON(),
+      url: (extent) => `/api/geocover?bbox=${extent.join(",")}`,
+      strategy: bboxstrategy, //lädt dateien nur für sichtbare bereiche
+    }),
+    visible: false, //startet unsichtbar
+  });
+};
 
 // Swisstopo Layer
 export const swisstopoLayer = new TileLayer({
