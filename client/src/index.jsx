52a7a50b7a810e6165ad4e27e320568cbf97315b
Gian Schneider <gianschneider12@bluewin.ch>
2025-05-06 11:55:38 +0200

    bereinigung index

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import OpenlayersPage from "./pages/openlayers/OpenlayersPage.jsx";
 import { HashRouter, Navigate, Route, Routes } from "react-router";
 import Header from "./Header.jsx";
-import MaplibrePage from "./pages/maplibre/MaplibrePage.jsx";
-import SpatialAnalysisPage from "./pages/spatialanalysis/SpatialAnalysisPage.jsx";
-import GeoTIFFPage from "./pages/geotiff/GeoTIFFPage.jsx";
 import BasemapPage from "./pages/basemap/BasemapPage.jsx";
 
 createRoot(document.getElementById("root")).render(
@@ -15,10 +11,6 @@ createRoot(document.getElementById("root")).render(
       <Header />
       <Routes>
         <Route path="/" element={<Navigate to="/basemap" replace />} />
-        <Route path="openlayers" element={<OpenlayersPage />} />
-        <Route path="maplibre" element={<MaplibrePage />} />
-        <Route path="spatialanalysis" element={<SpatialAnalysisPage />} />
-        <Route path="geotiff" element={<GeoTIFFPage />} />
         <Route path="basemap" element={<BasemapPage />} />
       </Routes>
     </HashRouter>
