edcff916dbe0d346625cbe1442e07973cbef2bb0
Gian Schneider <gianschneider12@bluewin.ch>
2025-05-20 16:13:36 +0200

    k

---
+++ docs/beispieldaten/fillData.sql
@@ -0,0 +1,11 @@
+INSERT INTO Haltestellen (
+    Name, Nummer, Transportunternehmen_Nummer, Transportunternehmen_Abkuerzung,
+    Betriebspunkttyp_Code, Betriebspunkttyp_Bezeichnung,
+    Verkehrsmittel_Code, Verkehrsmittel_Bezeichnung,
+    Gemeinde_Nummer, Gemeinde_Name, E, N, H
+) VALUES
+('Huttwil', 8508187, 16, 'BLS', 'VPG', 'Haltestelle und Bedienpunkt', 'B', 'Zug', 954, 'Huttwil', 2630659, 1218245, 638),
+('Ferenbalm-Gurbrue', 8504485, 16, 'BLS', 'VPG', 'Haltestelle und Bedienpunkt', 'B', 'Zug', 662, 'Ferenbalm', 2583271, 1200354, 494),
+('Waltensburg/Vuorz', 8509174, 154, 'RhB FR VR', 'VPG', 'Haltestelle und Bedienpunkt', 'B', 'Zug', 3981, 'Breil/Brigels', 2728430, 1181196, 744),
+('Villeneuve VD', 8501303, 1, 'SBB CFF FFS', 'VPG', 'Haltestelle und Bedienpunkt', 'B', 'Zug', 5414, 'Villeneuve (VD)', 2560650, 1138686, 375),
+('Domat/Ems', 8509181, 154, 'RhB FR VR', 'VPG', 'Haltestelle und Bedienpunkt', 'B', 'Zug', 3722, 'Domat/Ems', 2753678, 1188861, 581);
